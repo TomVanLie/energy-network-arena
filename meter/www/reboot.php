5a3cee5646ba9c644b1e3430498ec8b6e0a5e83d
TomVanLie <34542884+TomVanLie@users.noreply.github.com>
2023-11-08 17:09:36 +0100

    reboot.php go back fix

--- meter/www/reboot.php
+++ meter/www/reboot.php
@@ -1,6 +1,4 @@
 <?php
-if(shell_exec('sudo systemctl restart datel')){
-    echo 'Success. <a href="index.php">Go Back</a>';
-}
-
+shell_exec('sudo systemctl restart datel');
+echo 'Success. <a href="index.php">Go Back</a>';
 ?>
