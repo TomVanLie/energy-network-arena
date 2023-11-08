8d4dcfe888019f0ba176c6888408b5ead55d6602
TomVanLie <34542884+TomVanLie@users.noreply.github.com>
2023-11-08 15:39:17 +0100

    output state folder fix

--- meter/service.sh
+++ meter/service.sh
@@ -4,5 +4,7 @@
 JAR_FILE="/var/energy-network-arena/meter/DATEL-1.4.0-Server-osf.jar"
 TEST_OBJECTS_FILE="/var/energy-network-arena/meter/Corinex-Cloned-Custom.xml"
 SERVER_CONFIG_FILE="/var/energy-network-arena/meter/serverConf.xml"
+OUTPUT_STATE_FOLDER="/var/energy-network-arena/meter/www"
 
-/usr/bin/java -jar "$JAR_FILE" SO -w=1 -f "$TEST_OBJECTS_FILE" -c "$SERVER_CONFIG_FILE" -v -r -osf www
+
+/usr/bin/java -jar "$JAR_FILE" SO -w=1 -f "$TEST_OBJECTS_FILE" -c "$SERVER_CONFIG_FILE" -v -r -osf "$OUTPUT_STATE_FOLDER"
